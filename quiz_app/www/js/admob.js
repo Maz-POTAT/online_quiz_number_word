f934f56e7e5cc8d780df4d22d8bb6562e475689a
tuktarov9898 <84121020+Maz-POTAT@users.noreply.github.com>
2021-05-31 19:58:53 +0300

    change

--- quiz_app/www/js/admob.js
+++ quiz_app/www/js/admob.js
@@ -1,21 +1,21 @@
 var admobid = {};
 if( /(android)/i.test(navigator.userAgent) ) { 
     admobid = { // for Android
-        banner: 'ca-app-pub-3940256099942544/6300978111',
-        interstitial: 'ca-app-pub-3940256099942544/8691691433',
-        rewarded: 'ca-app-pub-3940256099942544/8691691433'
+        banner: 'ca-app-pub-6093857782627159/5731646477',
+        interstitial: 'ca-app-pub-6093857782627159/4418564808',
+        rewarded: 'ca-app-pub-6093857782627159/5091490402'
     };
 } else if(/(ipod|iphone|ipad)/i.test(navigator.userAgent)) {
     admobid = { // for iOS
-        banner: 'ca-app-pub-6869992474017983/4806197152',
-        interstitial: 'ca-app-pub-6869992474017983/7563979554',
-        rewarded: 'ca-app-pub-3940256099942544/5224354917'
+        banner: 'ca-app-pub-6093857782627159/5731646477',
+        interstitial: 'ca-app-pub-6093857782627159/4418564808',
+        rewarded: 'ca-app-pub-6093857782627159/5091490402'
     };
 } else {
     admobid = { // for Windows Phone
-        banner: 'ca-app-pub-6869992474017983/8878394753',
-        interstitial: 'ca-app-pub-6869992474017983/1355127956',
-        rewarded: 'ca-app-pub-3940256099942544/5224354917'
+        banner: 'ca-app-pub-6093857782627159/5731646477',
+        interstitial: 'ca-app-pub-6093857782627159/4418564808',
+        rewarded: 'ca-app-pub-6093857782627159/5091490402'
     };
 }
 
